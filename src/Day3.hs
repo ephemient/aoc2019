95090bab9978fe77add7235dbdc6b51dbcf64218
Daniel Lin <ephemient@gmail.com>
2019-12-03 12:11:53 -0500

    Day 3: Speed up by using lifted HashTable.ST instead of Map

--- src/Day3.hs
+++ src/Day3.hs
@@ -2,14 +2,15 @@
 Module:         Day3
 Description:    <https://adventofcode.com/2019/day/3 Day 3: Crossed Wires>
 -}
-{-# LANGUAGE FlexibleContexts, TupleSections, TypeApplications, TypeFamilies, ViewPatterns #-}
+{-# LANGUAGE FlexibleContexts, LambdaCase, TupleSections, TypeApplications, TypeFamilies, ViewPatterns #-}
 module Day3 (day3a, day3b) where
 
 import Control.Arrow (first, second)
-import Control.Monad (foldM_)
-import Control.Monad.Writer.Strict (execWriter, tell)
+import Control.Monad.Writer.Strict (MonadTrans, execWriterT, lift, tell)
+import Control.Monad.ST (ST, runST)
+import qualified Data.HashTable.ST.Basic as HT (delete, insert, lookup, new)
 import Data.Functor (($>))
-import qualified Data.Map.Strict as Map (alterF, empty)
+import Data.Hashable (Hashable)
 import Data.Semigroup (Semigroup(..), stimesIdempotentMonoid)
 import Text.Megaparsec (MonadParsec, ParseErrorBundle, choice, option, parse, sepEndBy)
 import Text.Megaparsec.Char (char, space1)
@@ -29,22 +30,24 @@ newtype Min a = Min { getMin :: Maybe a }
 instance (Ord a) => Semigroup (Min a) where
     Min Nothing <> y = y
     x <> Min Nothing = x
-    Min (Just x) <> Min (Just y) = Min . Just $! min x y
+    Min (Just x) <> Min (Just y) = Min $! Just $! min x y
     stimes = stimesIdempotentMonoid
 instance (Ord a) => Monoid (Min a) where mempty = Min Nothing
 
-intersections :: (Monad m, Ord k) =>
-    (k -> a -> Maybe b -> m (Maybe b)) -> [[(k, a)]] -> m ()
-intersections f =
-    foldM_ follow Map.empty . concat . zipWith @Int (zip . repeat) [0..] where
-    follow m (i, (k, a)) = Map.alterF f' k m where
-        f' old
-          | Just (j, _) <- old, i == j = pure old
-          | otherwise = fmap (i,) <$> f k a (snd <$> old)
+intersections :: (MonadTrans t, Monad (t (ST s)), Hashable k, Eq k) =>
+    (k -> a -> Maybe b -> t (ST s) (Maybe b)) -> [[(k, a)]] -> t (ST s) ()
+intersections f walks = do
+    field <- lift HT.new
+    let follow (i, (k, a)) = lift (HT.lookup field k) >>= \case
+            Just (j, _) | i == j -> return ()
+            old -> do
+                new <- fmap (i,) <$> f k a (snd <$> old)
+                lift $ maybe (HT.delete field k) (HT.insert field k) new
+    mapM_ follow (concat $ zipWith @Int (zip . repeat) [0..] walks)
 
 day3a :: String -> Either (ParseErrorBundle String ()) (Maybe Int)
-day3a input = getMin . execWriter . intersections f .
-    map (map (, ())) <$> parse parser "" input where
+day3a = fmap go . parse parser "" where
+    go x = getMin $ runST $ execWriterT $ intersections f $ map (, ()) <$> x
     f (0, 0) _ _ = pure Nothing
     f (manhattan -> d) _ Nothing = pure $ Just d
     f (manhattan -> d) _ (Just o) =
@@ -52,8 +55,8 @@ day3a input = getMin . execWriter . intersections f .
     manhattan (x, y) = abs x + abs y
 
 day3b :: String -> Either (ParseErrorBundle String ()) (Maybe Int)
-day3b input = getMin . execWriter . intersections f .
-    map (flip zip [0..]) <$> parse (parser @Int) "" input where
+day3b = fmap go . parse (parser @Int) "" where
+    go x = getMin $ runST $ execWriterT $ intersections f $ flip zip [0..] <$> x
     f (0, 0) _ _ = pure Nothing
     f _ d Nothing = pure $ Just d
     f _ d (Just o) = tell (Min $! Just $! d + o) $> (Just $! min d o)
