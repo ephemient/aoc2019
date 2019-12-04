64037d5e4dc551c2e785ee5f5cd3d8e58607921e
Daniel Lin <ephemient@gmail.com>
2019-12-04 17:28:32 -0500

    Day 1: Ignore low mass leading to negative fuel

--- src/main/kotlin/io/github/ephemient/aoc2019/Day1.kt
+++ src/main/kotlin/io/github/ephemient/aoc2019/Day1.kt
@@ -1,14 +1,11 @@
 package io.github.ephemient.aoc2019
 
 class Day1(private val lines: List<String>) {
-    private fun fuel(weight: Int): Int = weight / 3 - 2
+    private fun fuel(weight: Int): Int? = (weight / 3 - 2).takeIf { it > 0 }
 
-    private fun fuels(weight: Int): Int =
-        generateSequence(fuel(weight), ::fuel)
-            .takeWhile { it > 0 }
-            .sum()
+    private fun fuels(weight: Int): Int = generateSequence(fuel(weight), ::fuel).sum()
 
-    fun part1(): Int = lines.sumBy { fuel(it.toInt()) }
+    fun part1(): Int = lines.sumBy { fuel(it.toInt()) ?: 0 }
 
     fun part2(): Int = lines.sumBy { fuels(it.toInt()) }
 }
