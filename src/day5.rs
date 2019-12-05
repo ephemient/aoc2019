da81b80e6d91a9327d0f6cc16951029cc7f132d5
Daniel Lin <ephemient@gmail.com>
2019-12-05 16:07:08 -0500

    Day 5: Sunny with a Chance of Asteroids

---
+++ src/day5.rs
@@ -0,0 +1,117 @@
+use super::util;
+use std::error;
+use std::fmt;
+
+#[derive(Clone, Debug)]
+struct Error {
+    msg: String,
+}
+
+impl Error {
+    fn new(msg: String) -> Error {
+        Error { msg: msg }
+    }
+}
+
+impl fmt::Display for Error {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "error: {}", self.msg)
+    }
+}
+
+impl error::Error for Error {
+    fn source(&self) -> Option<&(dyn error::Error + 'static)> {
+        None
+    }
+}
+
+fn step<I, O>(mem: &mut [i32], ip: &mut usize, input: &mut I, output: &mut O) -> Result<bool, Error>
+where
+    I: FnMut() -> i32,
+    O: FnMut(i32),
+{
+    let op = mem[*ip];
+    let arg = |n: u32| -> usize {
+        match op / 10i32.pow(n + 1) % 10 {
+            0 => mem[*ip + n as usize] as usize,
+            _ => *ip + n as usize
+        }
+    };
+    match op % 100 {
+        1 => {
+            mem[arg(3)] = mem[arg(1)] + mem[arg(2)];
+            *ip += 4;
+        }
+        2 => {
+            mem[arg(3)] = mem[arg(1)] * mem[arg(2)];
+            *ip += 4;
+        }
+        3 => {
+            mem[arg(1)] = input();
+            *ip += 2;
+        }
+        4 => {
+            output(mem[arg(1)]);
+            *ip += 2;
+        }
+        5 => {
+            *ip = match mem[arg(1)] {
+                0 => *ip + 3,
+                _ => mem[arg(2)] as usize,
+            }
+        }
+        6 => {
+            *ip = match mem[arg(1)] {
+                0 => mem[arg(2)] as usize,
+                _ => *ip + 3,
+            }
+        }
+        7 => {
+            mem[arg(3)] = i32::from(mem[arg(1)] < mem[arg(2)]);
+            *ip = *ip + 4;
+        }
+        8 => {
+            mem[arg(3)] = i32::from(mem[arg(1)] == mem[arg(2)]);
+            *ip = *ip + 4;
+        }
+        99 => return Ok(false),
+        _ => return Err(Error::new(format!("bad opcode: {}", op))),
+    }
+    return Ok(true);
+}
+
+fn run<I, O>(mem: &mut [i32], mut input: I, mut output: O) -> Result<(), Error>
+where
+    I: FnMut() -> i32,
+    O: FnMut(i32),
+{
+    let mut ip: usize = 0;
+    while step(mem, &mut ip, &mut input, &mut output)? {}
+    return Ok(());
+}
+
+pub fn part1<'a, I, S>(lines: I) -> Result<i32, Box<dyn error::Error + Send + Sync>>
+where
+    I: IntoIterator<Item = &'a S>,
+    S: AsRef<str> + 'a,
+{
+    let line = lines.into_iter().nth(0).ok_or(util::Error)?.as_ref();
+    let mut mem: Vec<i32> =
+        util::parse_many(&line.split(',').map(|s| s.clone()).collect::<Vec<&str>>())?;
+    let mut output: Option<i32> = None;
+    run(&mut mem, || 1, |value| output = Some(value))?;
+    return Ok(output.ok_or_else(|| Error::new("no output".to_string()))?);
+}
+
+pub fn part2<'a, I, S>(lines: I) -> Result<i32, Box<dyn error::Error + Send + Sync>>
+where
+    I: IntoIterator<Item = &'a S>,
+    S: AsRef<str> + 'a,
+{
+    let line = lines.into_iter().nth(0).ok_or(util::Error)?.as_ref();
+    let mut mem: Vec<i32> =
+        util::parse_many(&line.split(',').map(|s| s.clone()).collect::<Vec<&str>>())?;
+    let mut output: Option<i32> = None;
+    run(&mut mem, || 5, |value| output = Some(value))?;
+    return Ok(output.ok_or_else(|| Error::new("no output".to_string()))?);
+}
--- src/main.rs
+++ src/main.rs
@@ -10,6 +10,7 @@ mod day1;
 mod day2;
 mod day3;
 mod day4;
+mod day5;
 mod util;
 
 build_const!("aoc2019.rs");
@@ -46,6 +47,13 @@ fn main() -> io::Result<()> {
         println!("Day 4");
         println!("{:?}", day4::part1(DAY4).map_err(util::to_ioerror)?);
         println!("{:?}", day4::part2(DAY4).map_err(util::to_ioerror)?);
+        println!();
+    }
+
+    if args.is_empty() || args.contains("5") {
+        println!("Day 5");
+        println!("{:?}", day5::part1(DAY5).map_err(util::to_ioerror)?);
+        println!("{:?}", day5::part2(DAY5).map_err(util::to_ioerror)?);
         println!();
     }
 
