bed73021176813b121822cdb130804cafc33c90e
Daniel Lin <ephemient@gmail.com>
2019-12-11 16:20:19 -0500

    Generalize over more Vector instances

--- src/Day11.hs
+++ src/Day11.hs
@@ -2,7 +2,7 @@
 Module:         Day11
 Description:    <https://adventofcode.com/2019/day/11 Day 11: Space Police>
 -}
-{-# LANGUAGE FlexibleContexts, NamedFieldPuns, RecordWildCards #-}
+{-# LANGUAGE FlexibleContexts, NamedFieldPuns, RecordWildCards, TypeApplications #-}
 module Day11 (day11a, day11b) where
 
 import Control.Monad.ST (runST)
@@ -12,31 +12,32 @@ import Data.List (intercalate)
 import Data.Map.Strict (Map)
 import qualified Data.Map.Strict as Map (filter, findWithDefault, insert, keysSet, singleton, size)
 import qualified Data.Set as Set (elems, member)
-import Data.Vector.Unboxed (Unbox, Vector, fromList)
+import Data.Vector.Generic (Vector, fromList)
+import qualified Data.Vector.Unboxed as Unboxed (Vector)
 import Intcode (Context(..), Memory(..), step)
 import Intcode.Vector (memory)
 import Text.Megaparsec (MonadParsec, ParseErrorBundle, parse, sepBy)
 import Text.Megaparsec.Char (char, space)
 import Text.Megaparsec.Char.Lexer (decimal, signed)
 
-parser :: (Integral e, Unbox e, MonadParsec err String m) => m (Vector e)
+parser :: (Vector v e, Integral e, MonadParsec err String m) => m (v e)
 parser = fromList <$> (signed (return ()) decimal `sepBy` char ',' <* space)
 
-data WalkState k i e = WalkState
+data WalkState k = WalkState
   { grid :: Map (k, k) Bool
   , position :: (k, k)
   , direction :: (k, k)
   }
 
-walk :: Bool -> Vector Int -> Map (Int, Int) Bool
+walk :: (Num k, Ord k, Vector v Int) => Bool -> v Int -> Map (k, k) Bool
 walk start mem0 = runST $ do
-    mem <- memory mem0
-    let liftedMemory = Memory
-          { readMem = lift . readMem mem
-          , writeMem = (.) lift . writeMem mem
+    Memory {..} <- memory mem0
+    let mem = Memory
+          { readMem = lift . readMem
+          , writeMem = (.) lift . writeMem
           }
         context0 = Context
-          { next = step liftedMemory context0
+          { next = step mem context0
           , output = \color input base ip -> do
                 walkState@WalkState {grid, position} <- get
                 put walkState {grid = Map.insert position (color /= 0) grid}
@@ -44,7 +45,7 @@ walk start mem0 = runST $ do
           , terminate
           }
         context1 = Context
-          { next = step liftedMemory context1
+          { next = step mem context1
           , output = \turn input base ip -> do
                 walkState@WalkState {..} <- get
                 let (x, y) = position
@@ -57,18 +58,19 @@ walk start mem0 = runST $ do
           , terminate
           }
         terminate _ _ _ = gets grid
-    evalStateT (step liftedMemory context0 [fromEnum start] 0 0) WalkState
+    evalStateT (step mem context0 [fromEnum start] 0 0) WalkState
           { grid = Map.singleton (0, 0) start
           , position = (0, 0)
           , direction = (0, 1)
           }
 
 day11a :: String -> Either (ParseErrorBundle String ()) Int
-day11a = fmap (Map.size . walk False) . parse parser ""
+day11a = fmap (Map.size . walk @Int @Unboxed.Vector False) . parse parser ""
 
 day11b :: String -> Either (ParseErrorBundle String ()) String
 day11b input = do
-    result <- Map.keysSet . Map.filter id . walk True <$> parse parser "" input
+    result <- Map.keysSet . Map.filter id . walk @Int @Unboxed.Vector True <$>
+        parse parser "" input
     let (xs, ys) = unzip $ Set.elems result
     return $ intercalate "\n"
       [ [ bool '\x2591' '\x2593' $ (x, y) `Set.member` result
--- src/Day9.hs
+++ src/Day9.hs
@@ -2,27 +2,28 @@
 Module:         Day9
 Description:    <https://adventofcode.com/2019/day/9 Day 9: Sensor Boost>
 -}
-{-# LANGUAGE FlexibleContexts, RecordWildCards #-}
+{-# LANGUAGE FlexibleContexts, RecordWildCards, TypeApplications #-}
 module Day9 (day9a, day9b) where
 
 import Control.Monad.ST (runST)
 import Data.List.NonEmpty (nonEmpty)
 import qualified Data.List.NonEmpty as NonEmpty (last)
-import Data.Vector.Unboxed (Unbox, Vector, fromList)
+import Data.Vector.Generic (Vector, fromList)
+import qualified Data.Vector.Unboxed as Unboxed (Vector)
 import Intcode.Vector (run)
 import Text.Megaparsec (MonadParsec, ParseErrorBundle, parse, sepBy)
 import Text.Megaparsec.Char (char, space)
 import Text.Megaparsec.Char.Lexer (decimal, signed)
 
-parser :: (Integral e, Unbox e, MonadParsec err String m) => m (Vector e)
+parser :: (Vector v e, Integral e, MonadParsec err String m) => m (v e)
 parser = fromList <$> (signed (return ()) decimal `sepBy` char ',' <* space)
 
 day9a :: String -> Either (ParseErrorBundle String ()) (Maybe Int)
 day9a input = fmap NonEmpty.last . nonEmpty <$> do
-    mem <- parse parser "" input
+    mem <- parse (parser @Unboxed.Vector) "" input
     return $ runST $ run mem [1]
 
 day9b :: String -> Either (ParseErrorBundle String ()) (Maybe Int)
 day9b input = fmap NonEmpty.last . nonEmpty <$> do
-    mem <- parse parser "" input
+    mem <- parse (parser @Unboxed.Vector) "" input
     return $ runST $ run mem [2]
