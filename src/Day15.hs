668dce1453fb2e02a3335cbf035c052a0696b27e
Daniel Lin <ephemient@gmail.com>
2019-12-15 18:03:32 -0500

    Day 15: Unify on one BFS

--- src/Day15.hs
+++ src/Day15.hs
@@ -2,22 +2,30 @@
 Module:         Day15
 Description:    <https://adventofcode.com/2019/day/15 Day 15: Oxygen System>
 -}
-{-# LANGUAGE FlexibleContexts, LambdaCase, TupleSections, TypeApplications, ViewPatterns #-}
+{-# LANGUAGE FlexibleContexts, TupleSections, TypeApplications, ViewPatterns #-}
 module Day15 (day15a, day15b) where
 
-import Control.Monad.ST (runST)
-import Data.Map.Lazy (Map)
-import qualified Data.Map.Lazy as Map (assocs, fromList, keysSet, mapWithKey, restrictKeys)
-import Data.Set (Set, (\\))
-import qualified Data.Set as Set (empty, insert, intersection, member, minView,null, singleton, toList, union, unions)
-import Data.Sequence (Seq(..), (><), (|>))
-import qualified Data.Sequence as Seq (fromList, singleton)
+import Control.Monad (filterM, when)
+import Control.Monad.Cont (callCC, runCont)
+import Control.Monad.RWS (evalRWS, evalRWST)
+import Control.Monad.State (get, gets, modify, put, runState, runStateT)
+import Control.Monad.Trans (lift)
+import Control.Monad.Writer (tell)
+import Data.Functor (($>))
+import Data.Functor.Identity (Identity(..))
+import Data.Heap (FstMinPolicy, Heap, HeapItem(..))
+import qualified Data.Heap as Heap (insert, singleton, view)
+import Data.List (foldl')
+import Data.Maybe (catMaybes)
+import Data.Semigroup (Last(..))
+import Data.Set (Set)
+import qualified Data.Set as Set (delete, fromList, insert, member, singleton)
 import Data.Vector.Generic (Vector)
 import qualified Data.Vector.Generic as Vector (fromList)
 import qualified Data.Vector.Unboxed as Unboxed (Vector)
 import Data.Void (Void)
-import Intcode (IntcodeT, evalIntcodeT, getOutput, setInput)
-import Intcode.Vector (memory)
+import Intcode (State(State), getOutput, runIntcodeT)
+import Intcode.Lazy (memory)
 import Text.Megaparsec (MonadParsec, ParseErrorBundle, parse, sepBy)
 import Text.Megaparsec.Char (char)
 import Text.Megaparsec.Char.Lexer (decimal, signed)
@@ -25,79 +33,61 @@ import Text.Megaparsec.Char.Lexer (decimal, signed)
 parser :: (Vector v e, Integral e, MonadParsec err String m) => m (v e)
 parser = Vector.fromList <$> signed (return ()) decimal `sepBy` char ','
 
-neighbors :: (Num e, Num a, Ord a) => (a, a) -> Map (a, a) e
-neighbors (x, y) = Map.fromList
-  [ ((x, y - 1), 1) -- north
-  , ((x, y + 1), 2) -- south
-  , ((x - 1, y), 3) -- west
-  , ((x + 1, y), 4) -- east
-  ]
+advance :: (Vector v e, Integral e, Monad m) =>
+    (v e, e, e) -> e -> m (Maybe e, (v e, e, e))
+advance (mem, base, ip) input = do
+    ((State _ base' ip', output), mem') <- flip runStateT mem $
+        runIntcodeT getOutput memory $ State (return input) base ip
+    return (output, (mem', base', ip'))
 
-findPath :: (Num e, Num a, Ord a) =>
-    Set (a, a) -> (a, a) -> (a, a) -> Seq ((a, a), e)
-findPath pass start end = findPath' pass $ Seq.singleton (start, Empty) where
-    findPath' pass' ((pos, path) :<| queue)
-      | pos == end = path
-      | otherwise = findPath' (pass' \\ Map.keysSet next) $
-            queue >< Seq.fromList (Map.assocs $ (path |>) <$> next) where
-            next = Map.mapWithKey (,) (neighbors pos) `Map.restrictKeys` pass'
-    findPath' _ _ = error "no path"
+bfsWithM :: (HeapItem pol item, Traversable t, Monad m) =>
+    (item -> m (t item)) -> Heap pol item -> m ()
+bfsWithM f = bfsWithM' where
+    bfsWithM' (Heap.view -> Just (item, queue)) = f item >>=
+        bfsWithM' . foldl' (flip Heap.insert) queue
+    bfsWithM' _ = return ()
 
-move :: (Monad m, Integral e) => e -> IntcodeT e m (Maybe e)
-move direction = do
-    setInput $ return direction
-    getOutput
+neighbors :: (Num e, Num a, Ord a) => (a, a) -> [((a, a), e)]
+neighbors (x, y) =
+    [((x, y - 1), 1), ((x, y + 1), 2), ((x - 1, y), 3), ((x + 1, y), 4)]
 
-navigate :: (Monad m, Num a, Ord a, Integral e) =>
-    Set (a, a) -> (a, a) -> (a, a) -> IntcodeT e m ((a, a), Maybe e)
-navigate pass start end =
-    navigate' (start, Nothing) $ findPath (Set.insert end pass) start end where
-    navigate' (pos, _) ((pos', direction) :<| rest) = do
-        result' <- move direction
-        case result' of
-            Just 1 -> navigate' (pos', result') rest
-            Just 2 -> navigate' (pos', result') rest
-            _ -> return (pos, result')
-    navigate' lastResult _ = return lastResult
+findOxygen :: (Vector v e, Integral e, Num a, Ord a, Monad m) =>
+    (a -> (a, a) -> m b) -> v e -> m (Set (a, a))
+findOxygen f mem0 = Set.fromList . snd <$> evalRWST
+    (bfsWithM step $ Heap.singleton @FstMinPolicy (1, ((0, 0), (mem0, 0, 0))))
+    () (Set.singleton (0, 0)) where
+    step (d, (pos, state)) = tell [pos] >>
+        catMaybes <$> mapM (tryStep d state) (neighbors pos)
+    tryStep d state (pos, direction) = do
+        visited <- get
+        if pos `Set.member` visited then return Nothing else
+            put (Set.insert pos visited) >>
+            case runIdentity $ advance state direction of
+                (Just 0, _) -> return Nothing
+                (Just 1, state') -> return $ Just (d + 1, (pos, state'))
+                (Just 2, state') ->
+                    lift (f d pos) $> Just (d + 1, (pos, state'))
+                _ -> error "unexpected response or termination"
 
-explore :: (Num a, Ord a, Integral e, Monad m) =>
-    IntcodeT e m (Maybe ((Set (a, a), (a, a))))
-explore = explore' (0, 0) Set.empty (Set.singleton (0, 0)) Nothing $
-    neighbors' (0, 0) where
-    explore' pos wall pass oxy (Set.minView -> Just (target, queue))
-      | target `Set.member` wall || target `Set.member` pass
-      = explore' pos wall pass oxy queue
-      | otherwise = navigate pass pos target >>= \case
-            (pos', Just 0) ->
-                explore' pos' (Set.insert target wall) pass oxy queue
-            (pos', Just 1) ->
-                explore' pos' wall (Set.insert pos' pass) oxy $
-                    Set.union queue $ neighbors' pos'
-            (pos', Just 2) | Nothing <- oxy ->
-                explore' pos' wall (Set.insert pos' pass) (Just pos') $
-                    Set.union queue $ neighbors' pos'
-            _ -> return Nothing
-    explore' _ _ pass (Just oxy) _ = return $ Just (pass, oxy)
-    explore' _ _ _ _ _ = return Nothing
-    neighbors' = Map.keysSet . neighbors @Int
-
-flood :: (Num a, Ord a) => Set (a, a) -> (a, a) -> [Set (a, a)]
-flood pass = takeWhile (not . Set.null) . fmap snd . iterate flood' .
-    (pass,) . Set.singleton where
-    grow = Set.unions . fmap (Map.keysSet . neighbors @Int) . Set.toList
-    flood' (pass', points) =
-        (pass' \\ points, Set.intersection pass' $ grow points \\ points)
+flood :: (Num a, Ord a) => Set (a, a) -> (a, a) -> Maybe Int
+flood pass start = fmap getLast $ snd $ evalRWS
+    (bfsWithM step $ Heap.singleton @FstMinPolicy (0, start)) () $
+    Set.delete start pass where
+    step (d, pos) = tell (Just $ Last d) >>
+        fmap (d + 1,) <$> filterM visit (fst <$> neighbors @Int pos)
+    visit pos = do
+        unvisited <- gets $ Set.member pos
+        when unvisited $ modify $ Set.delete pos
+        return unvisited
 
 day15a :: String -> Either (ParseErrorBundle String Void) (Maybe Int)
 day15a input = do
     mem0 <- parse (parser @Unboxed.Vector @Int) "" input
-    return $ fmap (length . ($ (0, 0)) . uncurry (findPath @Int)) $ runST $ do
-        mem <- memory mem0
-        evalIntcodeT (explore @Int) mem $ return 0
+    return $ (`runCont` id) $ callCC $ \exit ->
+        findOxygen (const . exit . Just) mem0 $> Nothing
 
 day15b :: String -> Either (ParseErrorBundle String Void) (Maybe Int)
 day15b input = do
     mem0 <- parse (parser @Unboxed.Vector @Int) "" input
-    return $ fmap (length . drop 1 . uncurry flood) $ runST $ do
-        mem <- memory mem0
-        evalIntcodeT (explore @Int) mem $ return 0
+    let (pass, oxygen) = runState (findOxygen (const $ put . Just) mem0) Nothing
+    return $ oxygen >>= flood @Int pass
