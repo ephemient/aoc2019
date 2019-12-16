d925c0a5cd4fa3e176805df81baf09c2717d8751
Daniel Lin <ephemient@gmail.com>
2019-12-16 12:47:03 -0500

    Day 16: In-place updates with fewer operations

--- src/day16.rs
+++ src/day16.rs
@@ -1,7 +1,7 @@
 use super::util;
 use std::char;
+use std::cmp;
 use std::error;
-use std::iter::Sum;
 
 pub fn part1<'a, I, S>(lines: I) -> Result<String, Box<dyn error::Error + Send + Sync>>
 where
@@ -18,20 +18,26 @@ where
         .map(|i| i as i32)
         .collect::<Vec<_>>();
     for _ in 0..100 {
-        value = (0..value.len())
-            .map(|i| {
-                i32::sum(value.iter().enumerate().map(|(j, x)| {
-                    let y = match (j + 1) % (4 * i + 4) / (i + 1) {
-                        1 => *x,
-                        3 => -x,
-                        _ => 0,
-                    };
-                    y
-                }))
+        for x in 0..value.len() {
+            let mut sign = true;
+            value[x] = (x..value.len())
+                .step_by(2 * x + 2)
+                .map(|base| {
+                    let span_sum: i32 = value[base..cmp::min(base + x + 1, value.len())]
+                        .iter()
+                        .sum();
+                    if sign {
+                        sign = false;
+                        -span_sum
+                    } else {
+                        sign = true;
+                        span_sum
+                    }
+                })
+                .sum::<i32>()
                 .abs()
-                    % 10
-            })
-            .collect();
+                % 10;
+        }
     }
     Ok(value
         .iter()
@@ -54,21 +60,24 @@ where
         .filter_map(|c| c.to_digit(10))
         .map(|i| i as i32)
         .collect::<Vec<_>>();
-    let logical_len = 10000 * value.len();
     let offset = value
         .iter()
         .take(7)
         .fold(0, |acc, x| 10 * acc + *x as usize);
-    assert!(offset < logical_len && logical_len <= 2 * offset);
-    value = (offset..logical_len)
-        .map(|i| value[i % value.len()])
+    let n = 10000 * value.len() - offset;
+    assert!(8 <= n && n < offset);
+    value = value
+        .iter()
+        .cloned()
+        .cycle()
+        .skip(offset % value.len())
+        .take(n)
         .collect();
     for _ in 0..100 {
-        for i in (1..value.len()).rev() {
-            value[i - 1] += value[i];
-        }
-        for i in value.iter_mut() {
-            *i = i.abs() % 10;
+        let mut acc = 0;
+        for i in (0..value.len()).rev() {
+            acc = (acc + value[i]).abs() % 10;
+            value[i] = acc;
         }
     }
     Ok(value
