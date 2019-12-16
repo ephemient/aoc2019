1aa3505a4253af0c14eb6b6746881297dd307b5e
Daniel Lin <ephemient@gmail.com>
2019-12-16 10:33:01 -0500

    Day 16: Speed up by removing most multiplication operations and replacing list with vector

--- src/Day16.hs
+++ src/Day16.hs
@@ -2,24 +2,32 @@
 Module:         Day16
 Description:    <https://adventofcode.com/2019/day/16 Day 16: Flawed Frequency Transmission>
 -}
-{-# LANGUAGE BangPatterns, TypeApplications, ViewPatterns #-}
+{-# LANGUAGE FlexibleContexts, TransformListComp, TypeApplications, ViewPatterns #-}
 module Day16 (day16a, day16b) where
 
+import Control.Monad (foldM, forM_)
 import Data.Char (digitToInt, intToDigit, isDigit)
 import Data.List (foldl')
-import qualified Data.Vector.Generic as Vector (fromListN, map, postscanr', take, toList)
+import Data.Vector.Generic (Vector)
+import qualified Data.Vector.Generic as Vector (fromList, fromListN, map, modify, postscanr', take, toList)
+import qualified Data.Vector.Generic.Mutable as MVector (length, read, write)
 import qualified Data.Vector.Unboxed as Unboxed (Vector)
 
-f :: [Int] -> [Int]
-f input =
-  [ abs (sum $ zipWith ($) fs input) `mod` 10
-  | (n, _) <- zip [1..] input
-  , let _:fs = cycle $ concatMap (replicate n) [const 0, id, const 0, negate]
-  ]
+f :: (Vector v e, Integral e) => v e -> v e
+f = Vector.modify $ \v -> let n = MVector.length v in forM_ [0..n - 1] $ \i ->
+    (`mod` 10) . abs <$> foldM (acc v) 0 (fns i n) >>= MVector.write v i where
+    acc v k (fn, i) = (+) k . fn <$> MVector.read v i
+    fns x n =
+      [ (fn, base + i)
+      | (base, fn) <- zip [x, 3 * x + 2..] $ cycle [id, negate]
+      , i <- [0..x]
+      , then takeWhile by base + i < n
+      ]
 
 day16a :: String -> [String]
 day16a (map digitToInt . filter isDigit -> input) =
-    map intToDigit . take 8 <$> iterate f input
+    map intToDigit . Vector.toList . Vector.take 8 <$> iterate f v0 where
+    v0 = Vector.fromList @Unboxed.Vector input
 
 day16b :: String -> [String]
 day16b (map digitToInt . filter isDigit -> input)
