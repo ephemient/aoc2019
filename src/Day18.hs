7e64a923e77881de5d18eb206f4ea495f42f18d6
Daniel Lin <ephemient@gmail.com>
2019-12-24 07:54:49 -0500

    Day 18: Marginal speedup from using Map operations to avoid some Heap operations

--- src/Day18.hs
+++ src/Day18.hs
@@ -6,7 +6,7 @@ Description:    <https://adventofcode.com/2019/day/18 Day 18: Many-Worlds Interp
 module Day18 (day18a, day18b) where
 
 import Common (bfsM, dijkstraM, neighbors)
-import Control.Monad (guard)
+import Control.Monad (filterM, guard, when)
 import Control.Monad.Cont (callCC, runCont)
 import Control.Monad.State (evalStateT, gets, modify)
 import Control.Monad.Trans (lift)
@@ -18,10 +18,10 @@ import Data.Heap (FstMinPolicy)
 import qualified Data.Heap as Heap (singleton)
 import Data.List hiding ((\\))
 import Data.Map.Lazy (Map, (!?))
-import qualified Data.Map.Lazy as Map (assocs, delete, filter, fromList, keys, mapMaybe, singleton, union, size)
+import qualified Data.Map.Lazy as Map (assocs, delete, empty, filter, fromList, insert, keys, lookup, mapMaybe, singleton, union, size)
 import Data.Maybe (maybeToList)
 import Data.Set (Set, (\\))
-import qualified Data.Set as Set (empty, foldr, fromList, insert, member, null, size)
+import qualified Data.Set as Set (empty, foldr, fromList, insert, null, size)
 
 mazePaths :: (Num a, Ord a, Ord b, Ord c) =>
     (c -> Bool) -> Map (a, a) (Maybe (Either b b)) -> Map (a, a) c -> Map c (Map c (Set b, Int))
@@ -42,7 +42,7 @@ mazePaths isTerminal maze locs = Map.fromList
 day18 :: (Num a, Ord a, Ord b) =>
     Map (a, a) (Maybe (Either b b)) -> [(a, a)] -> Maybe Int
 day18 maze start = flip runCont id $ callCC $ \exit ->
-    flip evalStateT Set.empty $ dijkstraM (go $ lift . exit . Just)
+    flip evalStateT Map.empty $ dijkstraM (go $ lift . exit . Just)
         (Heap.singleton @FstMinPolicy
             (0, (Left . fst <$> zip [0..] start, Set.empty))) $> Nothing where
     startLocs = Map.fromList . zip start $ Left <$> [0 :: Int ..]
@@ -50,8 +50,12 @@ day18 maze start = flip runCont id $ callCC $ \exit ->
     paths = mazePaths isRight maze $ Map.union startLocs keyLocs
     go exit (d, (_, keys)) | Set.size keys == Map.size keyLocs = exit d
     go _ (d, state@(poss, keys)) = do
-        seen <- gets (Set.member state)
-        if seen then return [] else modify (Set.insert state) $> do
+        seen <- gets $ maybe False (<= Left d) . Map.lookup state
+        if seen then return [] else modify (Map.insert state $ Left d) >>
+            let new (d', state') = do
+                    ok <- gets $ maybe True (> Right d') . Map.lookup state'
+                    when ok (modify . Map.insert state' $ Right d') $> ok
+            in filterM new $ do
             (pre, cur : post) <- zip (inits poss) (tails poss)
             (Right key, (doors, w)) <- maybeToList (paths !? cur) >>= Map.assocs
             guard . Set.null $ doors \\ keys
