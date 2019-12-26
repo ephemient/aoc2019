4f4060dd2dd867d3634f2bb242a4c3ef2d34d680
Daniel Lin <ephemient@gmail.com>
2019-12-26 01:42:01 -0500

    Day 19: style-o

--- src/main/kotlin/io/github/ephemient/aoc2019/Day19.kt
+++ src/main/kotlin/io/github/ephemient/aoc2019/Day19.kt
@@ -7,7 +7,7 @@ class Day19(lines: List<String>) {
     private fun f(x: Long, y: Long): Boolean =
         Intcode(ints.toMutableList()).runBlocking(listOf(x, y)).single() != 0L
 
-    fun part1(): Int = (0L..49L).sumBy { x -> (0L..49L).count{ y -> f(x, y) } }
+    fun part1(): Int = (0L..49L).sumBy { x -> (0L..49L).count { y -> f(x, y) } }
 
     fun part2(): Long {
         var x = 0L
