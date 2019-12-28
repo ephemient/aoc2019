0a101a2968d1b26c7c251de73055d6e2e48b5a55
Daniel Lin <ephemient@gmail.com>
2019-12-28 01:48:28 -0500

    Day 7: Switch from Array to Vector

--- src/Day7.hs
+++ src/Day7.hs
@@ -2,60 +2,46 @@
 Module:         Day7
 Description:    <https://adventofcode.com/2019/day/7 Day 7: Amplification Circuit>
 -}
-{-# LANGUAGE AllowAmbiguousTypes, FlexibleContexts, RecordWildCards, ScopedTypeVariables, TypeApplications #-}
+{-# LANGUAGE FlexibleContexts, RecordWildCards, TypeApplications, ViewPatterns #-}
 module Day7 (day7a, day7b, maxAmplify) where
 
-import Control.Monad.ST (ST, runST)
-import Data.Array.ST (MArray, STArray, newListArray, readArray, writeArray)
-import Data.Array.Unboxed  (IArray, UArray, bounds, elems, listArray)
-import Data.Ix (Ix)
+import Control.Monad.ST (runST)
 import Data.List (permutations)
 import qualified Data.List.NonEmpty as NonEmpty (last)
 import Data.Maybe (mapMaybe)
 import Data.List.NonEmpty (nonEmpty)
-import qualified Data.Map.Lazy as Map (foldlWithKey, singleton)
+import qualified Data.Map.Lazy as Map (empty, foldlWithKey, singleton)
+import qualified Data.Vector as Boxed (Vector)
+import Data.Vector.Generic (Vector)
+import qualified Data.Vector.Generic as Vector (convert, fromList)
+import qualified Data.Vector.Unboxed as Unboxed (Vector)
 import Data.Void (Void)
-import Intcode (Memory(..), run)
+import Intcode (run)
+import Intcode.Vector (memory)
 import Linear (Linear(..))
 import Text.Megaparsec (MonadParsec, ParseErrorBundle, parse, sepBy)
-import Text.Megaparsec.Char (char, space)
+import Text.Megaparsec.Char (char)
 import Text.Megaparsec.Char.Lexer (signed, decimal)
 
-parser :: (IArray a e, Integral e, MonadParsec err String m) => m (a Int e)
-parser = do
-    ints <- signed (return ()) decimal `sepBy` char ',' <* space
-    return $ listArray (0, length ints - 1) ints
+parser :: (Vector v e, Integral e, MonadParsec err String m) => m (v e)
+parser = Vector.fromList <$> signed (return ()) decimal `sepBy` char ','
 
-maxAmplify :: forall a b e i m. (MArray a (Linear e Int) m, IArray b e, Integral e, Ix i, Num i, Monad m) =>
-    b i e -> [e] -> m (Maybe e)
-maxAmplify mem phases = do
-    let vars = Linear @e 0 . flip Map.singleton 1 <$> [0..]
-        apply input Linear {..} =
-            Map.foldlWithKey expand (fromIntegral constant) variables where
-            expand c k b = c + fromIntegral b * input !! k
-        evaluate = foldl (map . apply) $ 0 : vars
-        resolve list = solution where solution = map (apply solution) list
-        run' phase = do
-            mem' <- newListArray @a @(Linear e Int) (bounds mem) $
-                fromIntegral <$> elems mem
-            run Memory
-              { readMem = readArray mem' . fromIntegral
-              , writeMem = writeArray mem' . fromIntegral
-              } $ fromIntegral phase : vars
-    amplifiers <- mapM run' phases
-    let outputs = resolve . evaluate <$> permutations amplifiers
-    return . fmap maximum . nonEmpty $ NonEmpty.last <$> mapMaybe nonEmpty outputs
+maxAmplify :: (Vector v e, Integral e) => v e -> [e] -> Maybe e
+maxAmplify (fmap (`Linear` Map.empty) . Vector.convert -> mem0) phases =
+    fmap maximum . nonEmpty $ NonEmpty.last <$> mapMaybe nonEmpty outputs where
+    vars = Linear 0 . flip Map.singleton 1 <$> [0..]
+    apply input Linear {..} =
+        Map.foldlWithKey expand (fromIntegral constant) variables where
+        expand c k b = c + fromIntegral b * input !! k
+    evaluate = foldl (map . apply) $ 0 : vars
+    resolve list = solution where solution = map (apply solution) list
+    run' phase = runST $
+        memory @Boxed.Vector mem0 >>= flip run (fromIntegral phase : vars)
+    amplifiers = run' <$> phases
+    outputs = resolve . evaluate <$> permutations amplifiers
 
 day7a :: String -> Either (ParseErrorBundle String Void) (Maybe Int)
-day7a input = do
-    mem <- parse (parser @UArray) "" input
-    let maxAmplify' :: forall s. [Int] -> ST s (Maybe Int)
-        maxAmplify' = maxAmplify @(STArray s) mem
-    return $ runST $ maxAmplify' [0..4]
+day7a input = flip maxAmplify [0..4] <$> parse (parser @Unboxed.Vector) "" input
 
 day7b :: String -> Either (ParseErrorBundle String Void) (Maybe Int)
-day7b input = do
-    mem <- parse (parser @UArray) "" input
-    let maxAmplify' :: forall s. [Int] -> ST s (Maybe Int)
-        maxAmplify' = maxAmplify @(STArray s) mem
-    return $ runST $ maxAmplify' [5..9]
+day7b input = flip maxAmplify [5..9] <$> parse (parser @Unboxed.Vector) "" input
--- test/Day7Spec.hs
+++ test/Day7Spec.hs
@@ -2,46 +2,43 @@
 module Day7Spec (spec) where
 
 import Day7 (maxAmplify)
-import Data.Array.IO (IOArray)
-import Data.Array.Unboxed (UArray, listArray)
-import Test.Hspec (Spec, describe, it, shouldReturn)
+import qualified Data.Vector.Unboxed as Vector (fromList)
+import Test.Hspec (Spec, describe, it, shouldBe)
 
 spec :: Spec
 spec = do
-    let makeMem :: [Int] -> UArray Int Int
-        makeMem ints = listArray (0, length ints - 1) ints
     describe "part 1" $ do
         it "example 1" $ do
-            let mem = makeMem
+            let mem = Vector.fromList
                   [ 3, 15, 3, 16, 1002, 16, 10, 16, 1, 16, 15, 15, 4, 15, 99, 0
                   , 0
                   ]
-            maxAmplify @IOArray mem [0..4] `shouldReturn` Just 43210
+            maxAmplify mem [0..4] `shouldBe` Just @Int 43210
         it "example 2" $ do
-            let mem = makeMem
+            let mem = Vector.fromList
                   [ 3, 23, 3, 24, 1002, 24, 10, 24, 1002, 23, -1, 23, 101, 5, 23
                   , 23, 1, 24, 23, 23, 4, 23, 99, 0, 0
                   ]
-            maxAmplify @IOArray mem [0..4] `shouldReturn` Just 54321
+            maxAmplify mem [0..4] `shouldBe` Just @Int 54321
         it "example 3" $ do
-            let mem = makeMem
+            let mem = Vector.fromList
                   [ 3, 31, 3, 32, 1002, 32, 10, 32, 1001, 31, -2, 31, 1007, 31
                   , 0, 33, 1002, 33, 7, 33, 1, 33, 31, 31, 1, 32, 31, 31, 4, 31
                   , 99, 0, 0, 0
                   ]
-            maxAmplify @IOArray mem [0..4] `shouldReturn` Just 65210
+            maxAmplify mem [0..4] `shouldBe` Just @Int 65210
     describe "part 2" $ do
         it "example 1" $ do
-            let mem = makeMem
+            let mem = Vector.fromList
                   [ 3, 26, 1001, 26, -4, 26, 3, 27, 1002, 27, 2, 27, 1, 27, 26
                   , 27, 4, 27, 1001, 28, -1, 28, 1005, 28, 6, 99, 0, 0, 5
                   ]
-            maxAmplify @IOArray mem [5..9] `shouldReturn` Just 139629729
+            maxAmplify mem [5..9] `shouldBe` Just @Int 139629729
         it "example 2" $ do
-            let mem = makeMem
+            let mem = Vector.fromList
                   [ 3, 52, 1001, 52, -5, 52, 3, 53, 1, 52, 56, 54, 1007, 54, 5
                   , 55, 1005, 55, 26, 1001, 54, -5, 54, 1105, 1, 12, 1, 53, 54
                   , 53, 1008, 54, 0, 55, 1001, 55, 1, 55, 2, 53, 55, 53, 4, 53
                   , 1001, 56, -1, 56, 1005, 56, 6, 99, 0, 0, 0, 0, 10
                   ]
-            maxAmplify @IOArray mem [5..9] `shouldReturn` Just 18216
+            maxAmplify mem [5..9] `shouldBe` Just @Int 18216
